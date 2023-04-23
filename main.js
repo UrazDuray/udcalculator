9011f9c6298d9f813a69586271f42e80a6322dd6
UrazDuray <cenkuraz@gmail.com>
2023-04-23 21:26:03 +0300

    Added color picker menu

--- main.js
+++ main.js
@@ -37,7 +37,7 @@ const createWindow = () => {
   //toolbarı kapa
   win.removeMenu()
   // DevTools'u aç.
-  //win.webContents.openDevTools()
+  win.webContents.openDevTools()
 };
 
 app.whenReady().then(() => {
