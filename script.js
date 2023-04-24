aa71160e2385988315d98efdd6cbda770694f30e
UrazDuray <cenkuraz@gmail.com>
2023-04-24 15:34:05 +0300

    Added Color Picker

--- script.js
+++ script.js
@@ -1463,8 +1463,6 @@ function ResistorBandStateChange(resistorId, choosing){
     
 }
 
-const rgba2hex = (rgba) => `#${rgba.match(/^rgba?\((\d+),\s*(\d+),\s*(\d+)(?:,\s*(\d+\.{0,1}\d*))?\)$/).slice(1).map((n, i) => (i === 3 ? Math.round(parseFloat(n) * 255) : parseFloat(n)).toString(16).padStart(2, '0').replace('NaN', '')).join('')}`
-
 function ResistorBandColorChosen(resistorId, color){
     ResistorBandColorUpdate(resistorId, color)
     CalculateResistorFromColor()
